8c61bffff470b240e550802dca2e6a0fc302443d
Jan Antensteiner <jan.antensteiner@gmail.com>
2023-07-16 17:31:24 +0300

    v.00001 live chat via webhooks

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    allowedHeaders: ['Content-Type'], // Specify the allowed headers
+  },
+});
+
+const PORT = 5001;
+
+// Keep track of usernames on chat to avoid duplicates
+const usernames = new Set();
+
+io.on('connection', (socket) => {
+  console.log('A new client connected.' + socket.id);
+
+  // Event handler for receiving new chat messages
+  socket.on('message', (message) => {
+    console.log('Received message:', message);
+
+    // Send the message to all connected clients
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    // Remove the disconnected client's username from the set by finding correct user via socket id
+    const username = [...usernames].find((username) => usernames[username] === socket.id);
+    if (username) {
+      console.log(`A client ${username} disconnected`);
+      usernames.delete(username);
+    }
+  });
+
+  // Event handler for setting username
+  socket.on('setUsername', (username) => {
+    // Check if the username is already taken
+    if (usernames.has(username)) {
+      socket.emit('usernameTaken');
+    } else {
+      // Store the username with the socket ID
+      usernames.add(username);
+      usernames[username] = socket.id;
+      socket.emit('usernameSet', username);
+    }
+  });
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
